0a32df1e5e0a68c00f19e0715e10974d9e54ead7
henry22 <henry811010@gmail.com>
2015-06-02 15:04:30 +0800

    Connect imageView and label to new class, adding an auto layout.

---
+++ Project10NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10NamesToFaces
+//
+//  Created by Henry on 6/2/15.
+//  Copyright (c) 2015 Henry. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
