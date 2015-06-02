f111b81d983032f15c835d29c5d3db002a7d7eb0
henry22 <henry811010@gmail.com>
2015-06-02 17:07:55 +0800

    The much more complicated delegate method is imagePickerController(_, didFinishPickingMediaWithInfo:), which returns when the user selected an image and it's being returned to you. This method needs to do several things: 1.Extract the image from the dictionary that is passed as a parameter. 2.Generate a unique filename for it. 3.Convert it to a JPEG, then write that JPEG to disk. 4.Dismiss the view controller.

--- Project10NamesToFaces/ViewController.swift
+++ Project10NamesToFaces/ViewController.swift
@@ -8,13 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "addNewPerson")
+    }
+    
+    func addNewPerson() {
+        let picker = UIImagePickerController()
+        //Allow the user to crop the picture they select
+        picker.allowsEditing = true
+        //Set self as the delegate, we need to conform not only the UIImagePickerControllerDelegate, but also the UINavigationControllerDelegate protocol
+        picker.delegate = self
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        //If the user cancels the image picker, we dismiss it
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    //The info dictionary parameter will contain one of two keys: UIImagePickerControllerEditedImage(the image that was edited) or UIImagePickerControllerOriginalImage,and it should only ever be the former unless changing the allowsEditing property
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        var newImage: UIImage
+        
+        //We can't extract straight into UIImages,because we don't know if these values exist as UIImages,so we need to use an optional method of typecasting as?, along with if/let syntax to make sure we always get the right thing out
+        if let possibleImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            newImage = possibleImage
+        } else if let possibleImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            newImage = possibleImage
+        } else {
+            return
+        }
+        
+        //We need to generate a unique filename for every image we import, so that we can copy it to our app's space on the disk without overwriting anything, and if the user ever deletes the picture from their photo library we still have our copy. NSUUID(), which generates a Universally Unique Identifier and is perfect for a random filename
+        let imageName = NSUUID().UUIDString
+        //This is used when working with file paths, and adds one string (imageName in our case) to a path, including whatever path separator is used on the platform
+        let imagePath = getDocumentsDirectory().stringByAppendingPathComponent(imageName)
+        //Once we have the image, we need to write it to disk. UIImageJPEGRepresentation() converts a UIImage to an NSData, and a quality value between 0 and 100
+        let jpegData = UIImageJPEGRepresentation(newImage, 80)
+        //There's a method on NSData called writeToFile() that writes its data to disk
+        jpegData.writeToFile(imagePath, atomically: true)
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func getDocumentsDirectory() -> String {
+        //Its first parameter asks for the documents directory, and its second parameter adds that we want the path to be relative to the user's home directory
+        let path = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as! [String]
+        //It nearly always contains only one thing: the user's documents directory, so we pull out the first element and return it
+        let documentsDirectory = path[0]
+        return documentsDirectory
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +77,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     //This must return an object of type UICollectionViewCell,and we already designed a prototype, and configured the PersonCell class for it,so we need to create and return these
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        //It creates a collection view cell using the reuse identified we specified, in this case "Person", this method will automatically try to reuse collection view cells, as soon as a cell scrolls out of view it can be recycled
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Person", forIndexPath: indexPath) as! PersonCell
         return cell
     }
